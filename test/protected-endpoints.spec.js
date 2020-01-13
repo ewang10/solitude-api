841b5ec03783d0e251d53c79c11e456609cb0298
Eric Wang <ericwang27759@gmail.com>
2020-01-13 16:17:45 -0500

    protected endpoints completed and tested

--- test/protected-endpoints.spec.js
+++ test/protected-endpoints.spec.js
@@ -2,7 +2,7 @@ const knex = require('knex');
 const app = require('../src/app');
 const helpers = require('./test-helpers');
 
-describe.only('Protected Endpoints', () => {
+describe('Protected Endpoints', () => {
     let db;
 
     before('make knex instance', () => {
